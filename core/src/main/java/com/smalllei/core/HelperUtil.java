de48f51eb53000f82753859276a73167091b12b4
smalllei <zxl_app@163.com>
2019-08-13 14:59:06 +0800

    add

---
+++ core/src/main/java/com/smalllei/core/HelperUtil.java
@@ -0,0 +1,16 @@
+package com.smalllei.core;
+
+import android.util.Log;
+
+/**
+ * @author: smalllei
+ * @date: 2019-08-13
+ * @time: 14:37
+ * @description:
+ * @change:
+ */
+public class HelperUtil {
+    public static void logE(String s){
+        Log.e("Log===",s);
+    }
+}
